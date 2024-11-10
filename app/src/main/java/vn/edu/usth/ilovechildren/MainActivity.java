6cfe959af2c84c6df8b3b273a18b0b21622e9295
minhnh296 <hacminh2004@gmail.com>
2024-11-10 10:54:11 +0700

    haha

--- app/src/main/java/vn/edu/usth/ilovechildren/MainActivity.java
+++ app/src/main/java/vn/edu/usth/ilovechildren/MainActivity.java
@@ -1,16 +1,24 @@
 package vn.edu.usth.ilovechildren;
 
 import android.os.Bundle;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import vn.edu.usth.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
