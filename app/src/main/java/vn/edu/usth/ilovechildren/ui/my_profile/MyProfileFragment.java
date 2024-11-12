e0ed0ee844efb197687ec3f46cc9e85d09230853
minhnh296 <hacminh2004@gmail.com>
2024-11-12 18:03:46 +0700

    h

--- app/src/main/java/vn/edu/usth/ilovechildren/ui/my_profile/MyProfileFragment.java
+++ app/src/main/java/vn/edu/usth/ilovechildren/ui/my_profile/MyProfileFragment.java
@@ -1,25 +1,21 @@
-package vn.edu.usth.ilovechildren.ui.dashboard;
+package vn.edu.usth.ilovechildren.ui.my_profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import vn.edu.usth.myapplication.R;
-import vn.edu.usth.myapplication.databinding.FragmentDashboardBinding;
 
-public class DashboardFragment extends Fragment {
+public class MyProfileFragment extends Fragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        return inflater.inflate(R.layout.fragment_my_profile, container, false);
     }
 }
