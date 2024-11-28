d963d4608491fe30ff5956d6e24114fdafaf3cd8
minhnh296 <hacminh2004@gmail.com>
2024-11-28 22:01:04 +0700

    newest

--- app/src/main/java/vn/edu/usth/ilovechildren/ui/my_profile/MyProfileFragment.java
+++ app/src/main/java/vn/edu/usth/ilovechildren/ui/my_profile/MyProfileFragment.java
@@ -7,7 +7,7 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-import vn.edu.usth.myapplication.R;
+import vn.edu.usth.ilovechildren.R;
 
 public class MyProfileFragment extends Fragment {
 
